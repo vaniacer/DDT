ea93348e199273ba9d5c45d2a6256ff9128c1a2d
Vaniac <ie.marov@gmail.com>
2018-04-04 08:36:06 +0300

    spaces

--- ddt.sh
+++ ddt.sh
@@ -47,54 +47,54 @@ db_error=(''
 [[ -f "$dbserr" ]] && rm "$dbserr"
 
 function download {
-	for ((j=0; j<10; j++)); do
+    for ((j=0; j<10; j++)); do
 
-		rsync -P -e ssh $addr:$bkpath/$dump $dmpdir/$localdump > /dev/null 2>> "$dlderr" \
+        rsync -P -e ssh $addr:$bkpath/$dump $dmpdir/$localdump > /dev/null 2>> "$dlderr" \
             && { printf "\nDownload complete."; return; }
-		sleep 5
+        sleep 5
 
-	done; printf "${dl_error[*]}"; cat "$dlderr"; continue
+    done; printf "${dl_error[*]}"; cat "$dlderr"; continue
 }
 
 function check {
-	for ((i=0; i<$N; i+=$C)); do printf '\n----------------------------------------------\n'
+    for ((i=0; i<$N; i+=$C)); do printf '\n----------------------------------------------\n'
 
-		read addr bkpath dbname dbtest <<< ${dbases[@]:$i:$C}
-		printf "Date\Time:\t$(date +'%d.%m.%Y %R')\n"
-		printf "DBServer:\t$addr\n"
-		printf "DBName:\t\t$dbname\n"
+        read addr bkpath dbname dbtest <<< ${dbases[@]:$i:$C}
+        printf "Date\Time:\t$(date +'%d.%m.%Y %R')\n"
+        printf "DBServer:\t$addr\n"
+        printf "DBName:\t\t$dbname\n"
 
-		dump=`ssh $addr ls $bkpath | grep ${dbname}_$mydate.*.gz` \
-			|| { printf "\nDump not found for the current date($mydate)!\n"; continue; }
-		localdump=${dbtest}_$mydate.gz
+        dump=`ssh $addr ls $bkpath | grep ${dbname}_$mydate.*.gz` \
+            || { printf "\nDump not found for the current date($mydate)!\n"; continue; }
+        localdump=${dbtest}_$mydate.gz
 
-		size=(`ssh $addr du   -m $bkpath/$dump`); size=${size[0]}
-		hash=(`ssh $addr $hasher $bkpath/$dump`); hash=${hash[0]}
+        size=(`ssh $addr du   -m $bkpath/$dump`); size=${size[0]}
+        hash=(`ssh $addr $hasher $bkpath/$dump`); hash=${hash[0]}
 
-		printf "RemoteFile:\t$bkpath/$dump ($size MB)\n"
-		printf "RemoteHash:\t$hash\n"
+        printf "RemoteFile:\t$bkpath/$dump ($size MB)\n"
+        printf "RemoteHash:\t$hash\n"
 
-		download
-		mysize=(`du   -m $dmpdir/$localdump`); mysize=${mysize[0]}
-		myhash=(`$hasher $dmpdir/$localdump`); myhash=${myhash[0]}
+        download
+        mysize=(`du   -m $dmpdir/$localdump`); mysize=${mysize[0]}
+        myhash=(`$hasher $dmpdir/$localdump`); myhash=${myhash[0]}
 
-		[[ $hash = $myhash ]] \
-			&& { printf " Hash checked!)\n\n"; } \
-			|| { printf "${dl_error[*]}\nLocalhash($myhash) not equal to remotehash($hash)!\n\n"; continue; }
+        [[ $hash = $myhash ]] \
+            && { printf " Hash checked!)\n\n"; } \
+            || { printf "${dl_error[*]}\nLocalhash($myhash) not equal to remotehash($hash)!\n\n"; continue; }
 
-		printf "LocalFile:\t$dmpdir/$localdump ($mysize MB)\n"
-		printf "LocalHash:\t$myhash\n"
+        printf "LocalFile:\t$dmpdir/$localdump ($mysize MB)\n"
+        printf "LocalHash:\t$myhash\n"
 
         dbconn="-h $dbhost -p $dbport -U $dbuser"
-		dropdb   $dbconn            $dbtest
-		createdb $dbconn -O $dbuser $dbtest
+        dropdb   $dbconn            $dbtest
+        createdb $dbconn -O $dbuser $dbtest
 
-		gunzip -c $dmpdir/$localdump | PGPASSWORD="$dbpass" psql -v ON_ERROR_STOP=1 $dbconn $dbtest > /dev/null \
+        gunzip -c $dmpdir/$localdump | PGPASSWORD="$dbpass" psql -v ON_ERROR_STOP=1 $dbconn $dbtest > /dev/null \
             2>> "$dbserr" || { printf "${db_error[*]}"; cat "$dbserr"; continue; }
 
-		printf "\nCheck complete!)\n"
+        printf "\nCheck complete!)\n"
 
-	done
+    done
 }
 
 check | mutt -s "$subjct" "$mailto"
