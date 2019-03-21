06cea2957600243499982020e29807fbedad1d68
Vaniac <ie.marov@gmail.com>
2019-03-21 14:55:18 +0300

    ref

--- ddt.sh
+++ ddt.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+dbases=(
+#-----------------------+-------------------+-----------------------------+--------------------------------------+
+#    Ssh alias(addr)    |Dump folder(bkpath)| Dump search pattern(dbname) | Test DB name(dbtest) Must be unique! |
+#-----------------------+-------------------+-----------------------------+--------------------------------------+
+#      'moscow'             '/backup'           'data_db_%d.%m.%Y'                   'moscow_data_prod_db'
+#      'rybinsk'            '/backup/new'       '%Y%m%d_db_data'                     'rybinsk_data_prod_db'
+#-----------------------+-------------------+-----------------------------+--------------------------------------+
+); N=${#dbases[*]}; C=4
+
 dmpdir=~/dumps                            # Dir to store dumps
 dlderr=~/logs/dlderr                      # Download errors file
 dbserr=~/logs/dbserr                      # DB test errors file
@@ -12,17 +21,7 @@ dbport=5432                               # DB server port
 dbuser=dbuser                             # User of test DB server
 dbpass=password                           # DB user password
 dbconf="-U $dbuser -h $dbhost -p $dbport" # DB connection parameters
-dbases=(
-#---------------------+-------------------+--------------------+--------------------------------------+
-#    Ssh alias(addr)  |Dump folder(bkpath)|  DB name(dbname)   | Test DB name(dbtest) Must be unique! |
-#---------------------+-------------------+--------------------+--------------------------------------+
-#Example:
-#    'moscow'           '/backup'           'data_db'            'moscow_data_prod_db'
-#    'rybinsk'          '/backup/new'       'data_db'            'rybinsk_data_prod_db'
-
-); N=${#dbases[*]}; C=4
-
-dl_error=(''
+dleror=(''
     ' ____  _____   ___   _  ____ \n'
     '|  _ \/ __\ \ / / \ | |/ ___|\n'
     '| |_) \___ \ V /|  \| | |    \n'
@@ -32,8 +31,7 @@ dl_error=(''
     '|  _|| |_) ||_) || | ||_) | |\n'
     '| |__|  _ <  _ < |_| | _ <|_|\n'
     '|_____|| \_\| \_\___/_| \_(_)\n')
-
-db_error=(''
+dberor=(''
     ' ____  ____ _____        _   \n'
     '|  _ \| __ )_   _|_  ___| |_ \n'
     '| | | |  _ \ | / _ \/ __| __|\n'
@@ -54,7 +52,7 @@ function download {
             && { printf "\nDownload complete."; return; }
         sleep 5
 
-    done; printf "${dl_error[*]}"; cat "$dlderr"; continue
+    done; printf "${dleror[*]}"; cat "$dlderr"; continue
 }
 
 function check {
@@ -70,26 +68,25 @@ function check {
 
         printf "Date\Time:\t$(date +'%d.%m.%Y %R')\n"
         printf "DBServer:\t$addr\n"
-        printf "DBName:\t\t$dbname\n"
 
-        dump=($(ssh $addr ls -t $bkpath | grep ${dbname}_.*$mydate.*.gz)) \
+        dump=( $(ssh $addr ls -t $bkpath | grep $(date +${dbname}).*.gz) )) \
             || { printf "\nDump not found for the current date($mydate)!\n"; continue; }
         dump=${dump[0]}
         localdump=${dbtest}_$mydate.gz
 
-        size=(`ssh $addr du   -m "$bkpath/$dump"`); size=${size[0]}
-        hash=(`ssh $addr $hasher "$bkpath/$dump"`); hash=${hash[0]}
+        size=( $(ssh $addr du   -m "$bkpath/$dump") ); size=${size[0]}
+        hash=( $(ssh $addr $hasher "$bkpath/$dump") ); hash=${hash[0]}
 
         printf "RemoteFile:\t$bkpath/$dump ($size MB)\n"
         printf "RemoteHash:\t$hash\n"
 
         download
-        mysize=(`du   -m "$dmpdir/$localdump"`); mysize=${mysize[0]}
-        myhash=(`$hasher "$dmpdir/$localdump"`); myhash=${myhash[0]}
+        mysize=( $(du   -m "$dmpdir/$localdump") ); mysize=${mysize[0]}
+        myhash=( $($hasher "$dmpdir/$localdump") ); myhash=${myhash[0]}
 
         [[ $hash = $myhash ]] \
             && { printf " Hash checked!)\n\n"; } \
-            || { printf "${dl_error[*]}\nLocalhash($myhash) not equal to remotehash($hash)!\n\n"; continue; }
+            || { printf "${dleror[*]}\nLocalhash($myhash) not equal to remotehash($hash)!\n\n"; continue; }
 
         printf "LocalFile:\t$dmpdir/$localdump ($mysize MB)\n"
         printf "LocalHash:\t$myhash\n"
@@ -100,7 +97,7 @@ function check {
         createdb $dbconf -O $dbuser $dbtest  > /dev/null 2>> "$dbserr"
 
         gunzip -c "$dmpdir/$localdump" | psql -v ON_ERROR_STOP=1 $dbconf $dbtest > /dev/null 2>> "$dbserr" \
-            || { printf "${db_error[*]}"; cat "$dbserr"; continue; }
+            || { printf "${dberor[*]}"; cat "$dbserr"; continue; }
 
         printf "\nCheck complete!)\n"
 
